0bbd5b5949202f12c558af454dc899752c9f2e3a
saif <saif.ansari@vetic.in>
2024-10-14 18:15:26 +0530

    changes

--- src/routes/orderbookRoutes.ts
+++ src/routes/orderbookRoutes.ts
@@ -2,11 +2,12 @@
 // src/routes/orderbookRoutes.ts
 
 import express from 'express';
-import { getOrderBook } from '../controllers/orderbookController';
+import { buyYesStock, getOrderBook } from '../controllers/orderbookController';
 
 const router = express.Router();
 
 // Define the GET endpoint for /orderbook
 router.get('/', getOrderBook);
+router.get('/buy/yes', buyYesStock);
 
 export default router;
