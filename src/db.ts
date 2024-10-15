8ff9c14f8d7ec571678f5c21f536fce528f7f341
saif <saif.ansari@vetic.in>
2024-10-16 01:10:16 +0530

    changes

--- src/db.ts
+++ src/db.ts
@@ -4,4 +4,4 @@ import { User, StockBalances } from "./models/user";
 export const INR_BALANCES: Record<string, User> = {};
 export const STOCK_BALANCES: StockBalances = {};
 export const ORDERBOOK: OrderBook = {};
-export const CLOSURE_AMOUNT : number = 10;
+export const CLOSURE_AMOUNT : number = 1000;
